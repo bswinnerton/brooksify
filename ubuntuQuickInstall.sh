a9ea72f6b0d741e2620ef1154997c6b8cccfda09
Local Administrator <albert@einstein.babblin.gs>
2012-12-06 17:51:58 +0000

    initial commit

---
+++ ubuntuQuickInstall.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo -n "FQDN: "
+read fqdn
+
+echo -n "IP: "
+read ip
+
+echo -n "Local Administrator name: "
+read ladmin
+
+regex='([a-z0-9]+)[\.]([a-z0-9]+)*\.([a-z]+)'
+if [[ $fqdn =~ $regex ]]; then
+    hostname=${BASH_REMATCH[1]}
+    domain=${BASH_REMATCH[2]}"."${BASH_REMATCH[3]}
+else
+    echo "Incorrect FQDN"
+fi
+
+sed -i "s/#force_color_prompt=yes/force_color_prompt=yes/g" ~/.bashrc
+source ~/.bashrc
+echo -e "\n\n# Added by ubuntuQuickInstall\n${ip} ${fqdn} ${hostname}" | sudo tee -a /etc/hosts
+
